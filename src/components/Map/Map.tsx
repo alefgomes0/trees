5297c6f58f6884974cf57d33f452a1ae7dd8db1a
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-08-11 20:05:47 -0300

    Same

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -10,8 +10,6 @@ type WorldMapProps = {
 }
 
 export const WorldMap = (props:WorldMapProps) => {
-  const API_KEY = import.meta.env.VITE_API_KEY
-  
   return (
     <Map
       initialViewState={{
@@ -20,7 +18,7 @@ export const WorldMap = (props:WorldMapProps) => {
         zoom: 2,
       }}
       style={{ width: "100%", height: "50vw" }}
-      mapStyle={`https://api.maptiler.com/maps/basic-v2/style.json?key=${API_KEY}`}
+      mapStyle="https://api.maptiler.com/maps/basic-v2/style.json?key=ZliihbZlV2InCpPBQDa1"
     >
       <NavigationControl position='top-right'/>
       <Markers wildfireData={props.wildfireData} wildfireQuantity={props.wildfireQuantity}/>
