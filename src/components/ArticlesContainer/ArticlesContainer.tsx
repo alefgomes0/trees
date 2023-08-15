860f6c6d1f24c96973e380be4a040298820f17c4
unknown <119986066+alefgomes0@users.noreply.github.com>
2023-08-15 14:57:26 -0300

    Change ArticlesContainer component

--- src/components/ArticlesContainer/ArticlesContainer.tsx
+++ src/components/ArticlesContainer/ArticlesContainer.tsx
@@ -5,16 +5,16 @@ export const ArticlesContainer = () => {
   return (
     <div className="grid grid-rows-[auto_1fr] grid-cols-1 px-2 sm:px-16 pt-8  pb-8 sm:pt-32 sm:pb-16 gap-6 sm:gap-16 text-[#031206] bg-[#e3ede6]">
       <h3 className="text-4xl sm:text-6xl font-IBMSerifMedium opacity-90">NEWS ABOUT US</h3>
-      <div className="grid grid-cols-1 sm:grid-cols-3 grid-rows-auto sm:grid-rows-1 grid-start-2 grid-end-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 grid-rows-auto md:grid-rows-1 grid-start-2 grid-end-3 gap-8">
         <div className="grid grid-rows-auto md:grid-rows-[min-content_150px_180px_auto] gap-4">
           <img
             src="../../../images/sebastian-unrau-sp-p7uuT0tw-unsplash.jpg"
-            className="sm:h-[300px] object-cover rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.2)]"
+            className="md:h-[300px] object-cover rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.2)]"
           />
           <h4 className="text-xl sm:text-2xl font-IBMSerifMedium">
             Hac habitasse platea dictumst sagittis purus.
           </h4>
-          <h6 className="text-sm sm:text-base opacity-[85%] sm:opacity-70 line-clamp-none md:line-clamp-3 lg:line-clamp-none">
+          <h6 className="text-sm sm:text-base opacity-[85%] sm:opacity-70 line-clamp-none sm:line-clamp-3 lg:line-clamp-none">
             Integer eget aliquet nibh praesent tristique. Donec pretium
             vulputate sapien nec sagittis aliquam malesuada bibendum. Laoreet
             non curabitur gravida arcu ac tortor dignissim convallis aenean.
@@ -27,15 +27,15 @@ export const ArticlesContainer = () => {
             </button>
           </Link>
         </div>
-        <div className="grid grid-rows-auto sm:grid-rows-[1fr_150px_180px_auto] gap-4">
+        <div className="grid grid-rows-auto md:grid-rows-[min-content_150px_180px_auto] gap-4">
           <img
             src="../../../images/nejc-soklic-XXp9RthTqCc-unsplash.jpg"
-            className="sm:h-[300px] object-cover rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.2)]"
+            className="md:h-[300px] object-cover rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.2)]"
           />
           <h4 className="text-2xl text-start font-IBMSerifMedium">
             Nibh ipsum consequat nisl vel pretium nisi vitae suscipit
           </h4>
-          <h6 className="text-sm sm:text-base opacity-[85%] sm:opacity-70 line-clamp-none md:line-clamp-3 lg:line-clamp-none">
+          <h6 className="text-sm sm:text-base opacity-[85%] sm:opacity-70 line-clamp-none sm:line-clamp-3 lg:line-clamp-none">
             Ullamcorper dignissim cras tincidunt lobortis. Nisl nisi scelerisque
             eu ultrices. Elit scelerisque mauris pellentesque pulvinar. Tortor
             aliquam nulla facilisi cras. Magna fermentum iaculis eu non diam.
@@ -48,15 +48,15 @@ export const ArticlesContainer = () => {
             </button>
           </Link>
         </div>
-        <div className="grid grid-rows-auto sm:grid-rows-[min-content_150px_180px_auto] gap-4">
+        <div className="grid grid-rows-auto md:grid-rows-[min-content_150px_180px_auto] gap-4">
           <img
             src="../../../images/fabian-jones-r_hgv8qEkwU-unsplash.jpg"
-            className="object-cover sm:h-[300px] rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.2)"
+            className="object-cover md:h-[300px] rounded shadow-[0_3px_3px_0_rgba(0,0,0,0.2)"
           />
           <h4 className="text-2xl font-IBMSerifMedium">
             Tristique senectus et netus et malesuada fames
           </h4>
-          <h6 className="text-sm sm:text-base opacity-[85%] sm:opacity-70 line-clamp-none md:line-clamp-3 lg:line-clamp-none">
+          <h6 className="text-sm sm:text-base opacity-[85%] sm:opacity-70 line-clamp-none sm:line-clamp-3 lg:line-clamp-4">
             Dignissim convallis aenean et tortor at risus viverra adipiscing.
             Est velit egestas dui id ornare arcu odio. Eget nullam non nisi est.
             Nibh cras pulvinar mattis nunc sed. Sed risus ultricies tristique
